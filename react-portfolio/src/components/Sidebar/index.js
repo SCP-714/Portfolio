090f72e79c55608b02daecfc98103b76eb3def26
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-03-07 20:48:53 -0600

    added links to personals

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoB from '../../assets/images/logo-b.png'
 import LogoSubtitle from '../../assets/images/logo_bain.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -12,16 +13,48 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt ="logo" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to='/'>
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            to='/'
+            >
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to='/about'>
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="about-link" 
+            to='/about'
+            >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to='/contact'>
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="contact-link" 
+            to='/contact'
+            >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target="_blank" 
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/bain-obermark-6357b6226/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+                </a>
+                <a 
+                target="_blank" 
+                rel="noreferrer"
+                href="https://github.com/SCP-714"
+                >
+                    <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
